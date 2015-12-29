a43af9c78249419a6e7d8623dbbf0527b3f5819b
miseler <github@miseler.de>
2015-12-30 00:03:37 +0100

    rudimentary logging

--- src/pocketride_activity.rb
+++ src/pocketride_activity.rb
@@ -3,14 +3,21 @@ require 'ruboto/util/toast'
 
 ruboto_import_widgets :Button, :LinearLayout, :TextView
 
-# http://xkcd.com/378/
+def log_e(msg)
+  android.util.Log.e "PocketRIDE", msg.to_s
+end
+
+def log(msg)
+  android.util.Log.d "PocketRIDE", caller[0].to_s
+  android.util.Log.i "PocketRIDE", msg.to_s
+end
 
 class PocketrideActivity
   def onCreate(bundle)
     super
-    set_title 'Domo arigato, Mr Ruboto!'
+    request_window_feature android.view.Window::FEATURE_NO_TITLE
 
-    self.content_view =
+    self.content_view = 
         linear_layout :orientation => :vertical do
           @text_view = text_view :text => 'What hath Matz wrought?', :id => 42, 
                                  :layout => {:width => :match_parent},
@@ -20,8 +27,9 @@ class PocketrideActivity
                  :id => 43, :on_click_listener => proc { butterfly }
         end
   rescue Exception
-    puts "Exception creating activity: #{$!}"
-    puts $!.backtrace.join("\n")
+    log_e $!.backtrace.join("\n")
+    log_e "Exception creating activity: #{$!}"
+    toast "LOGGED ERROR!"
   end
 
   private
