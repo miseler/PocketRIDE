55a79e3b566de6b7b68cc13839db5922e8fdb567
miseler <github@miseler.de>
2015-12-30 23:29:55 +0100

    dramatically improved logging by redirecting System.out&err so they get tagged with PocketRIDE

--- src/pocketride_activity.rb
+++ src/pocketride_activity.rb
@@ -5,14 +5,42 @@ def log_e(msg)
   android.util.Log.e "PocketRIDE", msg.to_s
 end
 
+def log_w(msg)
+  android.util.Log.v "PocketRIDE", caller[0].to_s
+  android.util.Log.w "PocketRIDE", msg.to_s
+end
+
 def log(msg)
-  android.util.Log.d "PocketRIDE", caller[0].to_s
+  android.util.Log.v "PocketRIDE", caller[0].to_s
   android.util.Log.i "PocketRIDE", msg.to_s
 end
 
+begin
+  require "java"
+  class SystemOutLogging < java.io.PrintStream
+    def println(line)
+      log_w line
+    end
+  end
+  class SystemErrLogging < java.io.PrintStream
+    def println(line)
+      log_e line
+      log_e $!.backtrace.join("\n")
+      log_e $!
+    end
+  end
+  java.lang.System.set_out SystemOutLogging.new java.lang.System.out
+  java.lang.System.set_err SystemErrLogging.new java.lang.System.err
+  java.lang.System.out.println "now logging System.out&err"
+rescue Exception
+  log_e $!.backtrace.join("\n")
+  log_e "Exception creating System.out&err logging: #{$!}"
+end
+
 class PocketrideActivity
-  def onCreate(bundle)
+  def on_create(bundle)
     super
+    log "on_create"
     request_window_feature android.view.Window::FEATURE_NO_TITLE
     self.content_view = R.layout.main
     
