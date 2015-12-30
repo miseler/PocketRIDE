a070beffd2672fd6bbf32f2b62b1b0a89adb9efc
miseler <github@miseler.de>
2015-12-30 18:00:47 +0100

    first button callbacks

--- src/pocketride_activity.rb
+++ src/pocketride_activity.rb
@@ -1,8 +1,6 @@
 require 'ruboto/widget'
 require 'ruboto/util/toast'
 
-ruboto_import_widgets :Button, :LinearLayout, :TextView
-
 def log_e(msg)
   android.util.Log.e "PocketRIDE", msg.to_s
 end
@@ -16,14 +14,18 @@ class PocketrideActivity
   def onCreate(bundle)
     super
     request_window_feature android.view.Window::FEATURE_NO_TITLE
-
     self.content_view = R.layout.main
+    
+    find_view_by_id(R.id.button_load).set_on_click_listener do
+      toast "LOAD!!!"
+    end
+    find_view_by_id(R.id.button_save).set_on_click_listener do
+      toast "SAVE!!!"
+    end
+    
   rescue Exception
     log_e $!.backtrace.join("\n")
     log_e "Exception creating activity: #{$!}"
     toast "LOGGED ERROR!"
   end
-
-  private
-
 end
